6ac226557fa7bf57db4dc4cd999fc836157d2e9d
Katie <katiebutler37@gmail.com>
2022-06-05 11:25:43 -0400

    set variable for current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 //Variable storing today's date
 var currentDay = moment().format("dddd, MMMM Do YYYY");
-//Display todays date in heading
+//Display todays date in header
 $("#currentDay").html(currentDay);
 
+var currentTime = moment().format("LTS");
+console.log(currentTime);
 
