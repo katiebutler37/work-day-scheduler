fb6b1e6ba081837633786d9f8def6b2cb3e2bfb5
Katie <katiebutler37@gmail.com>
2022-06-05 15:48:29 -0400

    debugging color coding background for description

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,45 @@ var currentDay = moment().format("dddd, MMMM Do YYYY");
 //Display todays date in header
 $("#currentDay").html(currentDay);
 
+//current hour in 24 hr time format
 var currentHour = moment().format("H");
 console.log(currentHour);
 
-//var description = 
+$(".24hr").each(function(){
+    var hour = $(this).text();
+    console.log(hour[0]);
+});
+
+// $(".time-block").each(function(hour) {
+//     hour = $(".24hr").text();
+//     $(".description").removeClass("past present future");
+
+//     // apply new class if task is near/over due date
+//     if (hour < currentHour) {
+//         $(".description").addClass("future");
+//     }
+//     else if (hour == currentHour) {
+//         $(".description").addClass("present");
+//   }
+//     else if (hour > currentHour) {
+//         $(".description").addClass("past");
+//     } 
+// });
 
 var checkHour = function() {
-    var hour = $(".24hr").find("id").text().trim();
-    console.log(hour);
-// remove any old classes from element
+    // remove any old classes from element
     $(".description").removeClass("past present future");
 
-    // apply new class if task is near/over due date
-    if (hour < currentHour) {
-        $(".description").addClass("future");
-    }
-    else if (hour == currentHour) {
-        $(".description").addClass("present");
-  }
-    else if (hour > currentHour) {
-        $(".description").addClass("past");
-    }
+        // apply new class if task is near/over due date
+        if (hour < currentHour) {
+            $(".description").addClass("future");
+        }
+        else if (hour == currentHour) {
+            $(".description").addClass("present");
+        }
+        else if (hour > currentHour) {
+            $(".description").addClass("past");
+        };
 }; 
 
 checkHour();
